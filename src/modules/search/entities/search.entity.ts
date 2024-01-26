7e74bae52744f9c194c57811cdf2abdf120d2b43
Onyetube Kosisochukwu <kosisochukwu@cbitindustries.com>
2024-01-26 21:58:04 +0000

    Fixed bugs

--- src/modules/search/entities/search.entity.ts
+++ src/modules/search/entities/search.entity.ts
@@ -2,11 +2,13 @@ import { ViewColumn, ViewEntity } from 'typeorm';
 import { Author } from '../../catalogs/types';
 
 @ViewEntity({
-  expression: `select 'books' as entity, bk.id, bk.title, bk."coverImageId" , bk."typeId", to_jsonb(bk.author) as author, ARRAY[]::varchar[]  as judges, bk."publicationDate"  from "${process.env.STAGE}-books" bk
+  expression: `select 'books' as entity, bk.id, bk.title, bk."coverImageId" , bk."typeId", to_jsonb(bk.author) as author, ARRAY[]::varchar[]  as judges, bk."publicationDate", bk."keyWords", null as "jurisdiction"   from "${process.env.STAGE}-books" bk
 union
-select 'articles' as entity, ar.id, ar.title, ar."coverImageId", ar."typeId", to_jsonb(ar.author) as author, ARRAY[]::varchar[]  as judge, ar."publicationDate"   from "${process.env.STAGE}-articles" ar
+select 'articles' as entity, ar.id, ar.title, ar."coverImageId", ar."typeId", to_jsonb(ar.author) as author, ARRAY[]::varchar[]  as judges, ar."publicationDate", ar."keyWords", null as "jurisdiction"   from "${process.env.STAGE}-articles" ar
 union
-select 'case-studies' as entity, cs.id, cs.title, cs."coverImageId", cs."typeId", null as author, cs.judges, cs."publicationDate"   from "${process.env.STAGE}-case-studies" cs`,
+select 'case-studies' as entity, cs.id, cs.title, cs."coverImageId", cs."typeId", null as author, cs.judges, cs."publicationDate", cs."keyWords", null as "jurisdiction"   from "${process.env.STAGE}-case-studies" cs
+union
+select 'legislation' as entity, le.id, le.title, le."coverImageId", le."typeId", null as author, ARRAY[]::varchar[]  as judges, le."publicationDate", le."keyWords", le."jurisdiction"   from "${process.env.STAGE}-legislation" le`,
 })
 export class Search {
   @ViewColumn()
@@ -32,4 +34,10 @@ export class Search {
 
   @ViewColumn()
   publicationDate?: Date;
+
+  @ViewColumn()
+  keyWords?: string;
+
+  @ViewColumn()
+  jurisdiction?: string;
 }
